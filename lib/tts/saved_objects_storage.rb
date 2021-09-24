125d0130739e0ff5ef58ef68f7aebd2952fa2af4
Duncan Uszkay <duncan.uszkay@shopify.com>
2021-09-24 18:29:43 -0400

    refactor saved objects storage

--- lib/tts/saved_objects_storage.rb
+++ lib/tts/saved_objects_storage.rb
@@ -13,66 +13,48 @@ module Tts
 		end
 
 		def save_map(map, name)
-			map_directory_path = File.join(session_path, MAP_DIRECTORY)
-
-			FileUtils.mkdir_p(map_directory_path)
-			
-			map_file_path = File.join(map_directory_path, "#{name}.json")
-
-			File.write(map_file_path, map)
+			in_saved_objects_directory(MAP_DIRECTORY) do
+				File.write("#{name}.json", map)
+			end
 		end
 
 		def save_character_set(set, set_name, set_thumbnail_path)
-			character_directory_path = File.join(session_path, CHARACTER_DIRECTORY)
-
-			FileUtils.mkdir_p(character_directory_path)
-			
-			character_set_file_path = File.join(character_directory_path, "#{set_name}.json")
-
-			File.write(character_set_file_path, set)
-
-			thumbnail_path = File.join(character_directory_path, "#{set_name}.png")
-
-			FileUtils.cp(set_thumbnail_path, thumbnail_path)
+			in_saved_objects_directory(CHARACTER_DIRECTORY) do
+				File.write("#{set_name}.json", set)
+				FileUtils.cp(set_thumbnail_path, "#{set_name}.png")
+			end
 		end
 
 		def save_background_set(set, set_name, set_thumbnail_path)
-			background_directory_path = File.join(session_path, BACKGROUND_DIRECTORY)
-
-			FileUtils.mkdir_p(background_directory_path)
-			
-			background_set_file_path = File.join(background_directory_path, "#{set_name}.json")
-
-			File.write(background_set_file_path, set)
-
-			thumbnail_path = File.join(background_directory_path, "#{set_name}.png")
-
-			FileUtils.cp(set_thumbnail_path, thumbnail_path)
+			in_saved_objects_directory(BACKGROUND_DIRECTORY) do
+				File.write("#{set_name}.json", set)
+				FileUtils.cp(set_thumbnail_path, "#{set_name}.png")
+			end
 		end
 
 		def save_item_set(set, set_name, set_thumbnail_path)
-			item_directory_path = File.join(session_path, ITEM_DIRECTORY)
-
-			FileUtils.mkdir_p(item_directory_path)
-			
-			item_set_file_path = File.join(item_directory_path, "#{set_name}.json")
-
-			File.write(item_set_file_path, set)
-
-			thumbnail_path = File.join(item_directory_path, "#{set_name}.png")
-
-			FileUtils.cp(set_thumbnail_path, thumbnail_path)
+			in_saved_objects_directory(ITEM_DIRECTORY) do
+				File.write("#{set_name}.json", set)
+				FileUtils.cp(set_thumbnail_path, "#{set_name}.png")
+			end
 		end
 
 		def save_files
-			file_directory_path = File.join(session_path, FILE_DIRECTORY)
-
-			FileUtils.mkdir_p(file_directory_path)
-			
-			FileUtils.cp_r(@session.files, file_directory_path)
+			in_saved_objects_directory(FILE_DIRECTORY) do
+				FileUtils.cp_r(@session.files, '.')
+			end
 		end
 
 		private
+
+		def in_saved_objects_directory(directory)
+			full_path = File.join(session_path, directory)
+
+			FileUtils.mkdir_p(full_path)
+			FileUtils.rm_rf("#{full_path}/.", secure: true)
+
+			Dir.chdir(full_path) { yield }
+		end
 
 		def session_path
 			File.join(base_path, @session.name)
